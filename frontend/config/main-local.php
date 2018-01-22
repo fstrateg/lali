011a1603141ce3492ae14016c57e4053887550a1
fstrateg <alexeym78@mail.ru>
2018-01-22 15:20:27 +0600

    init

---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'hl95ttQhcttlDFKHDE6CK7bWB0dCzA18',
+        ],
+    ],
+];
