5e0776923447045b0b61325a3f33385698cd16f6
fstrateg <alexeym78@mail.ru>
2018-02-15 13:09:57 +0600

    Отправка SMS сделана поправка на удаленные записи

--- common/config/main.php
+++ common/config/main.php
@@ -4,6 +4,7 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'timeZone'=>'Asia/Bishkek',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
@@ -13,7 +14,14 @@ return [
             [
                 'class'=>'common\components\Telegram',
                 'apiUrl'=>'https://api.telegram.org/',
-                'token'=>'??',
+                'token'=>'405775967:AAFNlCKgAhRUo_BH8nLV8IlegnjpNNBX1xk',
             ],
+        'yclients' =>
+        [
+            'class'=>'common\components\Yclients',
+            'token'=>'jw9gng6r8kggd54wdgh3',
+            'user'=>'e5638093ec3d59085e53012f6ac163e6',
+            'company'=>'31224',
+        ]
     ],
 ];
