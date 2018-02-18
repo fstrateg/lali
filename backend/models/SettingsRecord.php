f8e2b47467f6e03c5a77643546c96493826d938f
fstrateg <Alexeym78@mail.ru>
2018-02-19 00:12:24 +0600

    Настройки

---
+++ backend/models/SettingsRecord.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "settings".
+ *
+ * @property integer $id
+ * @property string $group
+ * @property string $param
+ * @property string $val
+ * @property string $name
+ */
+class SettingsRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'settings';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['group', 'param'], 'string', 'max' => 10],
+            [['val'], 'string', 'max' => 250],
+            [['name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'group' => 'Group',
+            'param' => 'Param',
+            'val' => 'Val',
+            'name' => 'Name',
+        ];
+    }
+}
